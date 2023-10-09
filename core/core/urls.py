156fe0e2be16955561c9a17121af977aa82d80cd
Julio Patiño <franjp132@gmail.com>
2023-10-09 11:53:24 -0500

    Configure url jwt

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,5 +1,9 @@
 from django.urls import include, path
 from django.contrib import admin
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 
 # Wire up our API using automatic URL routing.
@@ -11,4 +15,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]

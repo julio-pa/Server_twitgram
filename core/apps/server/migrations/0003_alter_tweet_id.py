dcf924cd80accda135abcf45507d3de508b8c9a2
Julio Patiño <franjp132@gmail.com>
2023-10-12 12:22:28 -0500

    change migrations

--- core/apps/server/migrations/0003_alter_tweet_id.py
+++ core/apps/server/migrations/0003_alter_tweet_id.py
@@ -10,9 +10,10 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
-            model_name='tweet',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
-        ),
+        # migrations.AlterField(
+        #     model_name='tweet',
+        #     name='id',
+        #     field=models.BigAutoField(
+        #         auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+        # ),
     ]
